a312f2555569b7f0f47cd2360d41640370ea6132
NosferJS <jsspokatashkin@gmail.com>
2022-01-13 13:10:07 +0300

    feat: fnction works

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    //let reverseNumber = n.toString();
+    let k = Math.abs(n);
+    k = k + '';
+ 
+     return Number(k.split('').reverse().join(''));
+   /*   let result = '';
+     for (let i = 0; i<n.length/2; i++) {
+         let first =  n[i];
+         let last = n[n.length-1];
+         result = 
+
+     } */
 }
